41da0cc50f7c6a2d13b581227b7c06d2fbff39c7
Your Name <you@example.com>
2025-06-11 09:12:49 +0300

    relationships task, status and category model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,10 +9,22 @@ class Task extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'due_date', 'user_id'];
+    protected $fillable = ['title', 'description', 'due_date', 'user_id', 'status_id', 'category_id'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
+    public function taskStatus()
+    {
+        return $this->belongsTo(taskStatus::class);
+    }
+
+    public function taskCategory()
+    {
+        return $this->belongsTo(taskCategory::class);
+    }
+
+    
 }
