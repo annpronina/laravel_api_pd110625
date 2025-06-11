ec3e351a066d8e3d83bbb3329301a692b0083d4e
Your Name <you@example.com>
2025-06-11 09:17:58 +0300

    comment model with migration and controller added

---
+++ app/Models/Comment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = ['content', 'task_id', 'user_id'];
+
+    public function task() {
+        return $this->belongTo(Task::class);
+    }
+}
