a8130a7ddc52a35972e94e70cd7f85ab6acb5610
Your Name <you@example.com>
2025-06-11 09:22:48 +0300

    migrations related to comments added

--- database/migrations/2025_06_11_062051_add_content_to_task.php
+++ database/migrations/2025_06_11_062051_add_content_to_task.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->id();
+        Schema::table('tasks', function (Blueprint $table) {
+            
             $table->string('content');
-            $table->foreignId('task_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->timestamps();
+            
         });
     }
 
@@ -25,6 +23,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('comments');
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
     }
 };
